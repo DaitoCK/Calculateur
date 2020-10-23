9a2aca5aad7c18a4fe70ef5b2b2f12776199754d
DaitoCK <72608496+DaitoCK@users.noreply.github.com>
2020-10-23 14:51:46 +0200

    m git commit -m

--- calculateur.php
+++ calculateur.php
@@ -4,11 +4,8 @@ if (isset($_GET['multi1'])){
     $mtp1 = $_GET['multi1'];
     $mtp2 = $_GET['multi2'];
 
-    if (isset($mtp1, $mtp2)){
-        $result = ($mtp1 * $mtp2);
-    } else {
-        echo "Le calcul n'est pas possible";
-    }
+    $result = ($mtp1 * $mtp2);
+
 
     if ($result === 0){
         $result = "";
@@ -23,7 +20,9 @@ if (isset($_GET['multi1'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+          crossorigin="anonymous">
     <title>Calcul</title>
 </head>
 
@@ -31,7 +30,9 @@ if (isset($_GET['multi1'])){
 <div class="container mt-5">
     <div class="card border-0 rounded-lg">
         <div class="card-body bg-light shadow-lg rounded-lg">
-            <h1> <?= $result; ?></h1>
+            <?php if(isset($result)):?>
+                <h1><?= $result ?></h1>
+            <?php endif; ?>
             <form method="GET" action="calculateur.php" >
                 <select class="custom-select" name="multi1">
                     <?php
